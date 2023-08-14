8e95ee561b345e237bbcbdc641f093f21f797153
sharif.ahmed <sharif@commlinkinfotech.com.bd>
2023-08-14 11:32:08 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let fullName = document.querySelector("#full-name");
+let userName = document.querySelector("#user-name");
+let email = document.querySelector("#email");
+let phoneNumber = document.querySelector("#phone-number");
+let password = document.querySelector("#password");
+let confirmPassword = document.querySelector("#confirm-password");
+let submitButton = document.querySelector("#btn-submit");
+
+
